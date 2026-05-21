8445a289e16422fdf1e786e43d6c3d14febd909f
Delicious233 <delicious233@hnu.edu.cn>
2026-05-21 21:50:32 +0800

    fix: remove textarea resize handle, dark scrollbar, clean orphan braces

--- backend/hub/client.go
+++ backend/hub/client.go
@@ -3,6 +3,7 @@ package hub
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"regexp"
 	"strings"
@@ -107,6 +108,8 @@ func (c *Client) ReadPump() {
 			c.handleTypingStart()
 		case "typing_stop":
 			c.handleTypingStop()
+		case "mark_read":
+			c.handleMarkRead()
 		default:
 			log.Printf("unknown message type: %s", msg.Type)
 		}
